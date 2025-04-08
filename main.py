4417048462a670fdf9bcbcb032140ef609f954e6
Minochoo <160795515+Minochoo@users.noreply.github.com>
2025-04-08 17:13:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -481,7 +481,7 @@ def format_timestamp(timestamp):
 def restart_program():
     p = psutil.Process(os.getpid())
     open_files = p.open_files()
-    connections = net_connections()
+    connections = connections()
     for handler in open_files:
         try:
             os.close(handler.fd)
